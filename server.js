06e43981d2fceb1a36b98efacda35a32eb2b13a1
CodinJohn <johnathon.collado@gmail.com>
2024-10-11 01:09:14 -0400

    bones coming along

--- server.js
+++ server.js
@@ -3,17 +3,20 @@ dotenv.config();
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-const testJWTRouter = require('./controllers/test-jwt');
-const usersRouter = require('./controllers/users');
-const profilesRouter = require('./controllers/profiles');
-const bcrypt = require('bcrypt');
-const User = require('./models/user');
+
 
 mongoose.connect(process.env.MONGODB_URI);
 
 mongoose.connection.on('connected', () => {
   console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
 });
+
+app.use(express.json());
+
+const testJWTRouter = require('./controllers/test-jwt');
+const usersRouter = require('./controllers/users');
+const profilesRouter = require('./controllers/profiles');
+
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
@@ -21,7 +24,6 @@ app.use((req, res, next) => {
   next()
 })
 
-app.use(express.json());
 app.use('/test-jwt', testJWTRouter);
 app.use('/users', usersRouter);
 app.use('/profiles', profilesRouter);
