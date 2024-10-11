97f05c72c3a3ed227654754e17555d6de12d99fd
CodinJohn <johnathon.collado@gmail.com>
2024-10-11 01:27:19 -0400

    Added Cors

--- server.js
+++ server.js
@@ -3,7 +3,10 @@ dotenv.config();
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-
+const testJWTRouter = require('./controllers/test-jwt');
+const usersRouter = require('./controllers/users');
+const profilesRouter = require('./controllers/profiles');
+const cors = require('cors')
 
 mongoose.connect(process.env.MONGODB_URI);
 
@@ -11,19 +14,15 @@ mongoose.connection.on('connected', () => {
   console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
 });
 
+
 app.use(express.json());
-
-const testJWTRouter = require('./controllers/test-jwt');
-const usersRouter = require('./controllers/users');
-const profilesRouter = require('./controllers/profiles');
-
+app.use(cors());
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   next()
 })
-
 app.use('/test-jwt', testJWTRouter);
 app.use('/users', usersRouter);
 app.use('/profiles', profilesRouter);
