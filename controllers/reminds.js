97d2e86779293eb2a0b17d978316139525dbfd3e
CodinJohn <johnathon.collado@gmail.com>
2024-10-11 03:35:57 -0400

    CRUD looking good moving to FrontEnd

--- controllers/reminds.js
+++ controllers/reminds.js
@@ -7,24 +7,60 @@ router.use(verifytoken)
 
 router.post('/', async (req, res) => {
     try {
-      const newRemind =await Remind.create({
-        ...req.body,
-        owner: req.user._id
-      });
-      res.status(201).json(newRemind);
+        const newRemind = await Remind.create({
+            ...req.body,
+            owner: req.user._id
+        });
+        res.status(201).json(newRemind);
     } catch (error) {
-      console.log(error);
-      res.status(500).json(error);
+        console.log(error);
+        res.status(500).json(error);
     }
-  });
-  
-router.get('/')
+});
 
+router.get('/:id', async (req, res) => {
+    try {
+        const remind = await Remind.findOne({ _id: req.params.id, owner: req.user._id });
+        if (!remind) {
+            return res.status(404).json({ error: 'Remind Me not found.' });
+        }
+        res.status(200), json(remind)
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ error: error.message });
 
+    }
+});
 
+router.put('/:id', async (req, res) => {
+    try {
+        const remind = await Remind.findOneAndUpdate(
+            { _id: req.params.id, ownder: req.user._id },
+            req.body,
+            { new: true }
+        );
+        if (!remind) {
+            return res.status(404).json({ error: 'Remind Me not found.' })
+        }
+        res.status(200).json(remind)
+    } catch (error) {
+        console.error(error)
+        res.status(500).json({ error: error.message })
+    }
+});
 
+router.delete('/:id', async (req, res) => {
+    try {
+        const remind = await Remind.findOneAndDelete({ _id: req.param.id, owner: req.user._id });
+        if (!remind) {
+            return res.status(404).json({ error: 'Remind Me not found.' });
+        }
+        res.status(200).json({ message: 'Remind Me deleted.' });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: error.message });
+    }
+});
 
-
-
-modules.exports = router;
+module.exports = router;
 
