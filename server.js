180574a4f842cfc07915164583db981a1ba118a5
CodinJohn <johnathon.collado@gmail.com>
2024-10-10 23:20:04 -0400

    init commit

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const app = express();
 const mongoose = require('mongoose');
 const testJWTRouter = require('./controllers/test-jwt');
 const usersRouter = require('./controllers/users');
+const profilesRouter = require('./controllers/profiles');
 const bcrypt = require('bcrypt');
 const User = require('./models/user');
 
@@ -13,11 +14,17 @@ mongoose.connect(process.env.MONGODB_URI);
 mongoose.connection.on('connected', () => {
   console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
 });
-
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  next()
+})
 
 app.use(express.json());
 app.use('/test-jwt', testJWTRouter);
 app.use('/users', usersRouter);
+app.use('/profiles', profilesRouter);
 // Routes go here
 
 app.listen(3000, () => {
